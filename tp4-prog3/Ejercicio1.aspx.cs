b778650bd8222f3c4513183a895273b94d682ff9
CoronelJJ <jjcoronel104@gmail.com>
2023-09-03 11:09:08 -0300

    Localidades cargadas en la ddl inicio

--- tp4-prog3/Ejercicio1.aspx.cs
+++ tp4-prog3/Ejercicio1.aspx.cs
@@ -18,12 +18,27 @@ namespace tp4_prog3
                 string rutaViajesSQL = "Data Source=localhost\\sqlexpress;Initial Catalog=Viajes;Integrated Security=True";
                 SqlConnection connection = new SqlConnection(rutaViajesSQL);
                 connection.Open();
-                SqlCommand cmd= new SqlCommand("Select * from Provincias", connection);
-                SqlDataReader dr = cmd.ExecuteReader();
+                SqlCommand cmdProvincias= new SqlCommand("Select * from Provincias", connection);
+                SqlDataReader dr = cmdProvincias.ExecuteReader();
+
+                //Provincia inicio
                 ddlProvinciaInicio.DataSource = dr;
                 ddlProvinciaInicio.DataTextField = "NombreProvincia";
                 ddlProvinciaInicio.DataValueField = "IdProvincia";
                 ddlProvinciaInicio.DataBind();
+
+                dr.Close();
+
+                //Localidad inicio
+                SqlCommand cmdLocalidades = new SqlCommand("Select * from Localidades",connection);
+                dr = cmdLocalidades.ExecuteReader();
+                ddlLocalidadInicio.DataSource = dr;
+                ddlLocalidadInicio.DataTextField = "NombreLocalidad";
+                ddlLocalidadInicio.DataValueField = "IdLocalidad";
+                ddlLocalidadInicio.DataBind();
+
+
+
             }
         }
 
