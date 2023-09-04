bbe28714167d62e13d643c0ee865c1e1a3ff4340
CoronelJJ <jjcoronel104@gmail.com>
2023-09-04 15:10:26 -0300

    Conexion dej EJ2 al server sql

--- tp4-prog3/Ejercicio2.aspx.cs
+++ tp4-prog3/Ejercicio2.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace tp4_prog3
 {
@@ -11,6 +12,20 @@ namespace tp4_prog3
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                EstablecerConexion(); 
+
+
+            }
+        }
+
+
+        void EstablecerConexion()
+        {
+            string rutaNeptunoSQL = "Data Source=localhost\\sqlexpress;Initial Catalog=Neptuno;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(rutaNeptunoSQL);
+            connection.Open();
 
         }
     }
